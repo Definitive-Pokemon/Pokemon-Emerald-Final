445e5b448ba8ca9c5596cb2f2db3bd79e29055a1
ElwinBran <elwinbran@gmail.com>
2021-04-21 20:05:47 +0200

    Fixed syntax error

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -836,7 +836,7 @@ static u8 GetTrainerBattleTransition(void)
             return B_TRANSITION_PHOEBE;
         if (gTrainerBattleOpponent_A == TRAINER_GLACIA_REMATCH_2)
             return B_TRANSITION_GLACIA;
-        if (gTrainerBattleOpponent_A == TRAINER_DRAKE_REMATCH_)2
+        if (gTrainerBattleOpponent_A == TRAINER_DRAKE_REMATCH_2)
             return B_TRANSITION_DRAKE;
         return B_TRANSITION_CHAMPION;
     }
